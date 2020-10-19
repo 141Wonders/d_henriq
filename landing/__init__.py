6170799c7a773bd86109fda7a7c0e285db807371
141Wonders <elliothenriq@gmail.com>
2020-10-19 17:58:16 -0500

    vimeo api communication

--- landing/__init__.py
+++ landing/__init__.py
@@ -1,5 +1,25 @@
+
 from flask import Flask
+import vimeo, uri
 
 app = Flask(__name__)
 
+client = vimeo.VimeoClient(
+  token='00806089b46ed18e0b6655aff77bc3ac',
+  key='9166539cbc5edc3a5ed3fb41a58fcc71b74776e1',
+  secret='qG+peLO+2N6K5k3OiXgqIB6efu3stPkFWfb1ptpkSCCHHnXoU5GupGqeIgDlbgEJXznqEZZg3VZNl3gRXagGny+Rk9yOfjgDtmbYqoymt77CFF4Tquk1MxWS8RbSsHY+'
+)
+
+# Make the request to the server for the "/me" endpoint.
+about_me = client.get('/me')
+
+# Make sure we got back a successful response.
+assert about_me.status_code == 200
+
+# Load the body's JSON data.
+print (about_me.json())
+
+
+
 from .views import *
+
